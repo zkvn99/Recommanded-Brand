98e5ebbd3474f31849044c7664d1de365bb8ff58
Minuk Lee <zkvn1103@naver.com>
2022-08-31 01:54:37 +0900

    html,css,js
    브랜드 검색 기능 및 검색 창 상단 고정 구현 디자인 수정 필요

--- html_css_demo/fixed.js
+++ html_css_demo/fixed.js
@@ -1,9 +1,12 @@
 var nav = document.getElementsByClassName("navbar");
+var search = document.getElementsByClassName("Search_container");
 
     window.onscroll = function sticky() {
       if(window.pageYOffset > nav[0].offsetTop) {
         nav[0].classList.add("header_fixed");
+        search[0].classList.add("Search_fixed");
       } else {
         nav[0].classList.remove("header_fixed");
+        search[0].classList.remove("Search_fixed");
       }
     }
