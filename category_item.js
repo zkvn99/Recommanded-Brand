460133aab206e58ba7b8c5e9f50f8e885e4ecd53
Minuk Lee <zkvn1103@naver.com>
2022-10-16 19:58:50 +0900

    item_page
    카테고리 기능 구현(javascript) 및 html, css 수정

---
+++ category_item.js
@@ -0,0 +1,29 @@
+let indicator = document.querySelector('.indicator').children;
+let item_list = document.querySelector('.item_content_grid').children;
+
+for(let i=0; i<indicator.length; i++)
+{
+    indicator[i].onclick = function(){
+        for(let x=0; x<indicator.length; x++)
+        {
+            indicator[x].classList.remove('active');
+        }
+        this.classList.add('active');
+        const displayItems = this.getAttribute('data-filter');
+        for(let z=0; z<item_list.length; z++)
+        {
+            item_list[z].style.transform = 'scale(0)';
+            setTimeout(()=>{
+                item_list[z].style.display = 'none';
+            }, 500);
+
+            if ((item_list[z].getAttribute('data-category') == displayItems) || displayItems == 'all')
+            {
+                item_list[z].style.transform = 'scale(1)';
+                setTimeout(()=>{
+                    item_list[z].style.display = 'block';
+                }, 500);  
+            }
+        }
+    }
+}
