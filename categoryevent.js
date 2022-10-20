c975acba6540ff1762c0abafd3244d327761bb6c
Minuk Lee <zkvn1103@naver.com>
2022-10-20 23:24:19 +0900

    shop_page
    모바일 카테고리 온클릭 형식 수정

--- categoryevent.js
+++ categoryevent.js
@@ -1,6 +1,11 @@
-const category_Btn = document.querySelector('.category_box');
+const mobile_category_Btn = document.querySelector('.mobile_category_box')
 const category_menu = document.querySelector('.indicator');
+const mobile_category_Ctn = document.querySelector('.indicator::after')
 
-category_Btn.addEventListener('click', () => {
+mobile_category_Btn.addEventListener('click', () => {
     category_menu.classList.toggle('active');
 });
+category_menu.addEventListener('click', () => {
+    category_menu.classList.remove('active');
+});
+
