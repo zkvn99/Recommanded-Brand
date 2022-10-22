2714452df7fefa2e1049fe67b13eafa365a71a52
Minuk Lee <zkvn1103@naver.com>
2022-10-23 02:48:43 +0900

    before delete

--- html_css_before/categoryevent.js
+++
@@ -1,6 +0,0 @@
-const category_Btn = document.querySelector('.category_box');
-const category_menu = document.querySelector('.indicator');
-
-category_Btn.addEventListener('click', () => {
-    category_menu.classList.toggle('active');
-});
